fe1525bb3ba7f1aab952c844c9df20e48e9825cd
Matt <mattep96@gmail.com>
2020-08-09 15:55:19 -0400

    Add typing indicator logic (show/hide will be replaced with websocket messages in the future)

--- src/app/chats/components/chat/chat.component.ts
+++ src/app/chats/components/chat/chat.component.ts
@@ -1,26 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {OutputMessage} from '../../models/output-message.model';
 import {ChatService} from '../../services/chat.service';
-import {Observable} from 'rxjs';
+import {Subject, Observable, Subscription} from 'rxjs';
+import {debounceTime, map, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnDestroy, OnInit {
 
   readonly isConnected$: Observable<boolean> = this.chatService.isConnected();
 
   readonly messages$: Observable<Array<OutputMessage>> = this.chatService.getMessages();
 
+  readonly _typingIndicator = new Subject<void>();
+
+  showTypingIndicator = false;
+
   currentUser: string;
 
   messageText: string;
 
+  private subscription = new Subscription();
+
   constructor(private chatService: ChatService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.subscription.add(
+      this._typingIndicator.pipe(
+        tap(() => this.showTypingIndicator = true),
+        debounceTime(1000)
+      ).subscribe(() => this.showTypingIndicator = false)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   connect(): void {
     this.chatService.connect();
