f9d3699d02d37441361d356a162b4f626d99be62
Matt <mattep96@gmail.com>
2020-08-09 18:59:18 -0400

    Add server managed typing indicators

--- src/app/chats/components/chat/chat.component.ts
+++ src/app/chats/components/chat/chat.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ChatService} from '../../services/chat.service';
 import {Subject, Observable, Subscription} from 'rxjs';
-import {debounceTime, tap} from 'rxjs/operators';
+import {debounceTime, distinctUntilChanged, map, shareReplay, tap} from 'rxjs/operators';
 import {ChatMessage} from '../../models/chat-message.model';
+import {User} from '../../models/user.model';
 
 @Component({
   selector: 'app-chat',
@@ -11,13 +12,49 @@ import {ChatMessage} from '../../models/chat-message.model';
 })
 export class ChatComponent implements OnDestroy, OnInit {
 
-  readonly isConnected$: Observable<boolean> = this.chatService.isConnected();
+  readonly isConnected$: Observable<boolean> = this.chatService.isConnected().pipe(
+    tap(connectionStatus => {
+      if (!!connectionStatus) {
+        connectionStatus.isConnected
+          ? this.chatService.sendUserConnectionMessage(this.currentUser)
+          : this.chatService.sendUserDisconnectionMessage(this.currentUser);
+      }
+    }),
+    map(connectionStatus => connectionStatus?.isConnected),
+    shareReplay()
+  );
 
   readonly messages$: Observable<Array<ChatMessage>> = this.chatService.getMessages();
 
-  readonly _typingIndicator = new Subject<void>();
+  readonly users$: Observable<Array<User>> = this.chatService.getUsers();
 
-  showTypingIndicator = false;
+  readonly typingUsers$: Observable<Array<User>> = this.users$.pipe(
+    map(users => users?.filter(user => user.typing && (user.name !== this.currentUser)))
+  );
+
+  readonly typingUsersMessage$: Observable<string> = this.typingUsers$.pipe(
+    map(users => {
+      let message = '';
+
+      if (!!users?.length) {
+        if (users.length > 3) {
+          message = 'Several users are typing...';
+        } else if (users.length === 1) {
+          message = `${users[0].name} is typing...`;
+        } else {
+          for (let i = 0; i < users.length - 1; i++) {
+            message += `${users[i].name}, `;
+          }
+
+          message = `${message.substring(0, message.length - 2)} and ${users[users.length - 1].name} are typing...`;
+        }
+      }
+
+      return message;
+    })
+  );
+
+  readonly _typingIndicator = new Subject<void>();
 
   currentUser: string;
 
@@ -30,9 +67,9 @@ export class ChatComponent implements OnDestroy, OnInit {
   ngOnInit(): void {
     this.subscription.add(
       this._typingIndicator.pipe(
-        tap(() => this.showTypingIndicator = true),
+        tap(() => this.chatService.sendUserTypingStartMessage(this.currentUser)),
         debounceTime(1000)
-      ).subscribe(() => this.showTypingIndicator = false)
+      ).subscribe(() => this.chatService.sendUserTypingEndMessage(this.currentUser))
     );
   }
 
--- src/app/chats/services/chat.service.ts
+++ src/app/chats/services/chat.service.ts
@@ -3,11 +3,15 @@ import {BehaviorSubject, Observable} from 'rxjs';
 import {ChatMessage} from '../models/chat-message.model';
 import {webSocket} from 'rxjs/webSocket';
 import {Message} from '../models/message.model';
+import {User} from '../models/user.model';
 
 enum MessageType {
   CONNECTION = 'connection',
+  USER_CONNECTED = 'user-connected',
+  USER_DISCONNECTED = 'user-disconnected',
   CHAT_MESSAGE = 'chat-message',
-  TYPING_INDICATOR = 'typing-indicator'
+  USER_TYPING_START = 'user-typing-start',
+  USER_TYPING_END = 'user-typing-end'
 }
 
 @Injectable({
@@ -15,21 +19,39 @@ enum MessageType {
 })
 export class ChatService {
 
-  private _connection = webSocket({
+  private readonly _connection = webSocket({
     url: 'ws://localhost:8080',
     openObserver: {
-      next: () => this._connected.next(true)
+      next: () => this._connected.next({ isConnected: true, sendMessage: true })
     },
     closeObserver: {
-      next: () => this._connected.next(false)
+      next: () => this._connected.next({ isConnected: true, sendMessage: true })
     }
   });
 
-  private _connected = new BehaviorSubject(false);
+  private readonly _connected = new BehaviorSubject<{ isConnected: boolean, sendMessage: boolean }>(null);
 
-  private _messages = new BehaviorSubject<Array<ChatMessage>>(null);
+  private readonly _messages = new BehaviorSubject<Array<ChatMessage>>(null);
+
+  private readonly _users = new BehaviorSubject<Array<User>>(null);
 
   constructor() { }
+
+  sendUserConnectionMessage(name: string): void {
+    this.sendMessage(MessageType.USER_CONNECTED, name);
+  }
+
+  sendUserDisconnectionMessage(name: string): void {
+    this.sendMessage(MessageType.USER_DISCONNECTED, name);
+  }
+
+  sendUserTypingStartMessage(name: string): void {
+    this.sendMessage(MessageType.USER_TYPING_START, name);
+  }
+
+  sendUserTypingEndMessage(name: string): void {
+    this.sendMessage(MessageType.USER_TYPING_END, name);
+  }
 
   sendChatMessage(message: ChatMessage): void {
     this.sendMessage(MessageType.CHAT_MESSAGE, message);
@@ -48,20 +70,29 @@ export class ChatService {
         const receivedMessage: Message = message as Message;
 
         if (!!receivedMessage.messageType && !!receivedMessage.payload) {
-          if (receivedMessage.messageType === MessageType.CONNECTION) {
-            this._messages.next(receivedMessage.payload);
-          } else if (receivedMessage.messageType === MessageType.CHAT_MESSAGE) {
-            const messages = this._messages.getValue();
-            messages.push(receivedMessage.payload);
-            this._messages.next(messages.sort((a, b) => a.time < b.time ? -1 : 1));
+          switch (receivedMessage.messageType) {
+            case MessageType.CONNECTION:
+              this._messages.next(receivedMessage.payload);
+              break;
+            case MessageType.USER_CONNECTED:
+            case MessageType.USER_DISCONNECTED:
+            case MessageType.USER_TYPING_START:
+            case MessageType.USER_TYPING_END:
+              this._users.next(receivedMessage.payload);
+              break;
+            case MessageType.CHAT_MESSAGE:
+              const messages = this._messages.getValue();
+              messages.push(receivedMessage.payload);
+              this._messages.next(messages.sort((a, b) => a.time < b.time ? -1 : 1));
+              break;
           }
         }
       },
       () => {
-        this._connected.next(false);
+        this._connected.next({ isConnected: false, sendMessage: false });
       },
       () => {
-        this._connected.next(false);
+        this._connected.next({ isConnected: false, sendMessage: false });
       }
     );
   }
@@ -71,11 +102,15 @@ export class ChatService {
     this._connection.complete();
   }
 
-  isConnected(): Observable<boolean> {
+  isConnected(): Observable<{ isConnected: boolean, sendMessage: boolean }> {
     return this._connected.asObservable();
   }
 
   getMessages(): Observable<Array<ChatMessage>> {
     return this._messages.asObservable();
+  }
+
+  getUsers(): Observable<Array<User>> {
+    return this._users.asObservable();
   }
 }
